40b2ec9084abf9828400890971c4419b5f7b4843
Olanewton <jolakanmi01@gmail.com>
2022-12-21 15:13:02 +0000

    Task 101 updated

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 import sys
 
-
 def safe_function(fct, *args):
     try:
         res = fct(*args)
@@ -10,5 +9,4 @@ def safe_function(fct, *args):
         print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
-        return
-
+        return res
